6b75c26a68c43fb86a88de77b8798df31537d415
Robert Herber <robert@kingstinct.com>
2023-10-24 02:45:21 +0200

    Make cli-runner accept apps as well

--- packages/core/cli-runner.ts
+++ packages/core/cli-runner.ts
@@ -1,26 +1,42 @@
 import path from 'node:path'
 
-import type { Configure, Plugin } from '@zemble/core'
+import type { Configure, Plugin, ZembleApp } from '@zemble/core'
 
 const pluginPaths = process.argv.slice(2)
 
-if (pluginPaths.length === 0) {
-  console.warn('No plugins specified, specify paths to them as arguments')
+if (pluginPaths[0] === '--help' || pluginPaths[0] === '-h' || pluginPaths[0] === 'help') {
+  console.log('Usage: zemble-dev <pathToPluginOrApp> [morePlugins..]')
+  process.exit(0)
 }
 
-type Runner = (config: Configure) => void
+if (pluginPaths.length === 0) {
+  console.warn('No plugins specified so this is just an empty app, see `zemble-dev --help` for usage')
+}
+
+type Runner = (config: Configure | ZembleApp) => void
 
 export const cliRunner = async (runner: Runner) => {
-  const plugins = await Promise.all(pluginPaths.map(async (p) => {
+  let app: ZembleApp | undefined
+  const files = await Promise.all(pluginPaths.map(async (p) => {
     const relativePath = path.isAbsolute(p)
       ? p
       : path.join(process.cwd(), p)
 
-    const plugin = (await import(relativePath)).default as Promise<Plugin>
+    const plugin = (await (await import(relativePath)).default) as Plugin | ZembleApp
+
+    if ('request' in plugin) {
+      app = plugin
+    }
+
     return plugin
   }))
 
-  return runner({ plugins })
+  // if there's an app, quit early
+  if (app) {
+    return runner(app)
+  }
+
+  return runner({ plugins: files.filter((f): f is Plugin => 'pluginName' in f) })
 }
 
 export default cliRunner
