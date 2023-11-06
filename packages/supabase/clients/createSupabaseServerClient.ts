11dd8d6c366999277e11d81faf7cd9241bd500e1
Robert Herber <robert@kingstinct.com>
2023-11-06 08:13:37 +0100

    Lets try localhost for CI

--- packages/supabase/clients/createSupabaseServerClient.ts
+++ packages/supabase/clients/createSupabaseServerClient.ts
@@ -6,7 +6,7 @@ import plugin from '../plugin'
 import type { CookieOptions } from '@supabase/ssr'
 import type { SupabaseClient } from '@supabase/supabase-js'
 
-const testSupabaseUrl = 'http://127.0.0.1:54321'
+const testSupabaseUrl = process.env.CI ? 'http://localhost:54321' : 'http://127.0.0.1:54321'
 const testSupabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wdHFtbWF4bXluYWhzZ2Z1dmhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwMzc3MzEsImV4cCI6MjAxNDYxMzczMX0.TEyzdP13ogqp6NGhSQYwtzOkwmuJDMkXUjU3gUVmN1c'
 const testConfig = {
   supabaseUrl: testSupabaseUrl,
