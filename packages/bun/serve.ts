95b01765cde87d08a40a452549777bc3dcb8c3e4
Robert Herber <robert@kingstinct.com>
2024-01-30 13:28:35 +0000

    Add websocket support for bun

--- packages/bun/serve.ts
+++ packages/bun/serve.ts
@@ -7,7 +7,19 @@ import type { Configure } from '@zemble/core'
 export const serve = async (config: Configure | Promise<Zemble.App> | Zemble.App) => {
   const app = await ('plugins' in config ? createApp(config) : config)
   await runBeforeServe(app)
-  const bunServer = Bun.serve({ fetch: app.hono.fetch })
+
+  const bunServer = Bun.serve({
+    fetch: async (request, server) => {
+    // Upgrade the request to a WebSocket
+      if (server.upgrade(request)) {
+        return new Response()
+      }
+
+      return app.hono.fetch(request, server)
+    },
+    // todo [>1]: support updating websockets handler without restarting the server
+    websocket: app.websocketHandler,
+  })
 
   // mostly for clickability in the terminal :)
   const linkPrefix = bunServer.hostname === 'localhost' ? 'http://' : ''
---
+++ packages/graphql/utils/createWebSocketHandler.ts
@@ -0,0 +1,48 @@
+import { makeHandler } from 'graphql-ws/lib/use/bun'
+
+import type { GraphQLSchemaWithContext, YogaServerInstance } from 'graphql-yoga'
+
+export const createWebsocketHandler = (
+  schema: GraphQLSchemaWithContext<Zemble.GraphQLContext>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  yoga: YogaServerInstance<Zemble.GraphQLContext, Record<string, any>>,
+) => {
+  const websocketHandler = makeHandler({
+    schema,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    execute: (args: any) => args.rootValue.execute(args),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    subscribe: (args: any) => args.rootValue.subscribe(args),
+    onSubscribe: async (ctx, msg) => {
+      const {
+        schema, execute, subscribe, contextFactory, parse, validate,
+      } = yoga.getEnveloped({
+        ...ctx,
+        req: ctx.extra.request,
+        socket: ctx.extra.socket,
+        params: msg.payload,
+      })
+
+      const args = {
+        schema,
+        operationName: msg.payload.operationName,
+        document: parse(msg.payload.query),
+        variableValues: msg.payload.variables,
+        contextValue: await contextFactory(),
+        rootValue: {
+          execute,
+          subscribe,
+        },
+      }
+
+      const errors = validate(args.schema, args.document)
+      if (errors.length) return errors
+
+      return args
+    },
+  })
+
+  return websocketHandler
+}
+
+export default createWebsocketHandler
