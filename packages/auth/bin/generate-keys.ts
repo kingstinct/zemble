cb86c09f84b0f4f55580ff90d3109636c68d36e1
Robert Herber <robert@kingstinct.com>
2024-02-08 16:06:00 +0000

    Update generate-keys

--- packages/auth/bin/generate-keys.ts
+++ packages/auth/bin/generate-keys.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 import zembleContext from '@zemble/core/zembleContext'
-import * as fs from 'node:fs/promises'
+import * as fs from 'node:fs'
 import * as path from 'node:path'
 
 import generateKeys from '../generate-keys'
@@ -8,14 +8,14 @@ import generateKeys from '../generate-keys'
 await generateKeys().then(async ({ publicKey, privateKey }) => {
   const envPath = path.join(process.cwd(), '.env')
 
-  if (!(await fs.exists(envPath))) {
-    await fs.writeFile(envPath, '')
+  if (!(fs.existsSync(envPath))) {
+    fs.writeFileSync(envPath, '')
   }
 
-  const data = await fs.readFile(envPath, 'utf8')
+  const data = fs.readFileSync(envPath, 'utf8')
 
   if (!data.includes('PUBLIC_KEY') && !data.includes('PRIVATE_KEY')) {
-    await fs.appendFile(envPath, `\nPUBLIC_KEY='${publicKey.trim()}'\nPRIVATE_KEY='${privateKey.trim()}'`)
+    fs.appendFileSync(envPath, `\nPUBLIC_KEY='${publicKey.trim()}'\nPRIVATE_KEY='${privateKey.trim()}'`)
     zembleContext.logger.info('PUBLIC_KEY and PRIVATE_KEY was appended to your local .env file!')
   } else {
     zembleContext.logger.info('The "PUBLIC_KEY" and/or "PRIVATE_KEY" already exists in .env file, will not overwrite!')
