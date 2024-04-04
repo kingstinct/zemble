07dd38e2970341e45ef5e414005e59fa7f38ee8d
Robert Herber <robert@kingstinct.com>
2024-04-04 17:26:08 +0200

    Cleanup

--- packages/auth-apple/codegen.ts
+++ packages/auth-apple/codegen.ts
@@ -1,16 +1,3 @@
-import mergeDeep from '@zemble/core/utils/mergeDeep'
-import defaultConfig, { defaultServerOutputPath } from '@zemble/graphql/codegen'
+import defaultConfig from '@zemble/graphql/codegen'
 
-import type { CodegenConfig } from '@graphql-codegen/cli'
-
-const config: CodegenConfig = {
-  generates: {
-    [defaultServerOutputPath]: {
-      config: {
-
-      },
-    },
-  },
-}
-
-export default mergeDeep<CodegenConfig>(defaultConfig, config)
+export default defaultConfig
