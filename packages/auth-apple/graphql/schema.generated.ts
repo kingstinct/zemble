0a7d3798cf40445cf4f45feb40dbbfaa680d55ad
Robert Herber <robert@kingstinct.com>
2024-04-26 18:47:43 +0200

    Eslint fixes

--- packages/auth-apple/graphql/schema.generated.ts
+++ packages/auth-apple/graphql/schema.generated.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import '@zemble/core'
 import type { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
+export type Maybe<T> = T | null | undefined;
+export type InputMaybe<T> = T | null | undefined;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
