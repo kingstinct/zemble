2a844c04cf5d6685e563defda84c8cbc98defee7
Ryan Iguchi <ryan.iguchi1@gmail.com>
2024-06-02 09:23:28 +0200

    lint

--- apps/minimal/app.ts
+++ apps/minimal/app.ts
@@ -1,5 +1,5 @@
-import Auth from '@zemble/auth'
-import AuthOtp from '@zemble/auth-otp'
+// import Auth from '@zemble/auth'
+// import AuthOtp from '@zemble/auth-otp'
 import Bull from '@zemble/bull'
 import { createApp } from '@zemble/core'
 import Resend from '@zemble/email-resend'
@@ -9,7 +9,7 @@ import Migrations from '@zemble/migrations'
 import dryrunAdapter from '@zemble/migrations/adapters/dryrun'
 import Logger from '@zemble/pino'
 import Routes from '@zemble/routes'
-import Elks from '@zemble/sms-46elks'
+// import Elks from '@zemble/sms-46elks'
 // import Twilio from '@zemble/sms-twilio'
 
 import MyRoutes from './plugins/files/plugin'
@@ -27,23 +27,23 @@ export default createApp({
     //   TWILIO_ACCOUNT_SID: process.env['TWILIO_ACCOUNT_SID'],
     //   TWILIO_AUTH_TOKEN: process.env['TWILIO_AUTH_TOKEN'],
     // }),
-    Elks.configure({
-      ELKS_USERNAME: process.env['ELKS_USERNAME'],
-      ELKS_PASSWORD: process.env['ELKS_PASSWORD'],
-      disable: false,
-      options: {
-        dryrun: 'yes',
-      },
-    }),
-    AuthOtp.configure({
-      fromSms: 'Kingstinct',
-      fromEmail: {
-        email: 'info@getlifeline.app',
-        name: 'Kingstinct',
-      },
-      smsMessage: 'Your two factor code is {{twoFactorCode}}',
-      WHITELISTED_COUNTRY_CODES: ['SE'],
-    }),
+    // Elks.configure({
+    //   ELKS_USERNAME: process.env['ELKS_USERNAME'],
+    //   ELKS_PASSWORD: process.env['ELKS_PASSWORD'],
+    //   disable: false,
+    //   options: {
+    //     dryrun: 'yes',
+    //   },
+    // }),
+    // AuthOtp.configure({
+    //   fromSms: 'Kingstinct',
+    //   fromEmail: {
+    //     email: 'info@getlifeline.app',
+    //     name: 'Kingstinct',
+    //   },
+    //   smsMessage: 'Your two factor code is {{twoFactorCode}}',
+    //   WHITELISTED_COUNTRY_CODES: ['SE'],
+    // }),
     GraphQLLogger,
     GraphQL.configure({
       outputMergedSchemaPath: './app.generated.graphql',
@@ -56,9 +56,9 @@ export default createApp({
       RESEND_API_KEY: 're_V3Znff2Z_C8jWM3NRxR4ERVc6xnFA7bxZ',
       disable: false,
     }),
-    Auth.configure({
-      PUBLIC_KEY: process.env['PUBLIC_KEY'],
-      PRIVATE_KEY: process.env['PRIVATE_KEY'],
-    }),
+    // Auth.configure({
+    //   PUBLIC_KEY: process.env['PUBLIC_KEY'],
+    //   PRIVATE_KEY: process.env['PRIVATE_KEY'],
+    // }),
   ],
 })
