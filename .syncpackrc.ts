de434f688de6aec71ea49e9c8e507705a2dbafa0
Robert Herber <robert@kingstinct.com>
2025-07-05 17:33:42 +0200

    lets try syncpack

--- .syncpackrc.ts
+++ .syncpackrc.ts
@@ -1,3 +1,11 @@
 export default {
   strict: true,
+  versionGroups: [
+    {
+      packages: ['**'],
+      dependencies: ['$LOCAL'],
+      dependencyTypes: ['dev', 'prod'],
+      pinVersion: 'workspace:*',
+    },
+  ],
 } satisfies import('syncpack').RcFile
