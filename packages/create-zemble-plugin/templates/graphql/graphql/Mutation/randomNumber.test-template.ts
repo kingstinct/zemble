69d3438e8dadc6350fe3f06b8558301fa0589930
Robert Herber <robert@kingstinct.com>
2025-07-05 18:49:42 +0200

    minor fixes

--- packages/create-zemble-plugin/templates/graphql/graphql/Mutation/randomNumber.test-template.ts
+++ packages/create-zemble-plugin/templates/graphql/graphql/Mutation/randomNumber.test-template.ts
@@ -2,18 +2,19 @@ import { expect, it } from 'bun:test'
 import { createTestApp } from '@zemble/core/test-utils'
 
 import plugin from '../../plugin'
-import { graphql } from '../client.generated'
 
-const randomNumberMutation = graphql(`
+// import from generated client // Replace with actual implementation
+
+const randomNumberMutation = `
   mutation RandomNumber {
     randomNumber
   }
-`)
+`
 
 it('Should return a number', async () => {
   const app = await createTestApp(plugin)
 
-  const response = await app.gqlRequest(randomNumberMutation, {})
+  const response = await app.gqlRequestUntyped(randomNumberMutation, {})
   expect(response.data).toEqual({
     randomNumber: expect.any(Number),
   })
